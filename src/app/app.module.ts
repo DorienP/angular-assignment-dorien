67ea8a9ae57a4fd3f423aab2e6951743fc277689
Dorien <dorien.pine@gmail.com>
2022-09-29 07:58:14 -0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { NewsComponent } from './news/news.component';
+import { ToolsComponent } from './tools/tools.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      { path: 'tasks', loadChildren: () => import('./tasks/tasks.module').then(m => m.TasksModule) },
+      { path: 'news', loadChildren: () => import('./news/news.module').then(m => m.NewsModule) },
+      { path: 'tools', loadChildren: () => import('./tools/tools.module').then(m => m.ToolsModule) },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
